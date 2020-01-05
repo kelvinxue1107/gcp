b8bcf5bb2896b55d6d53bb9d76b84b37bf120c33
kelvinxue1107 <kelvin.xue@outlook.com>
2020-01-05 20:00:47 +0800

    update network name from backup-network to backup

--- main.tf
+++ main.tf
@@ -14,13 +14,14 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
   network_interface {
-    # A default network is created for all GCP projects
+    
     network       = "${google_compute_network.vpc_network.self_link}"
     access_config { 
     }
   }
 }
+#define network name as backup
 resource "google_compute_network" "vpc_network" {
-  name                    = "backup-network"
+  name                    = "backup"
   auto_create_subnetworks = "true"
 }
